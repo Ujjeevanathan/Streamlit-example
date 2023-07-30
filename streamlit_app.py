3e1bbb2c4b840c5756ccbc80bd8c6e8a12d56e1e
Ujjeevanathan <rj.ujjeev@gmail.com>
2023-07-30 11:03:44 +0530

    First
    first summary

---
+++ streamlit_app.py
@@ -0,0 +1,33 @@
+import streamlit as stl
+ 
+# add title to your app
+ 
+stl.title("CloudThat ")
+
+#importing the library
+ 
+import streamlit as stl
+ 
+# adding title to your app
+ 
+stl.title("Cloudthat ")
+ 
+#adding text to your app
+ 
+stl.write("CloudThat incepted in 2012 is the first Indian organization to offer Cloud training and consultancy for mid-market and enterprise clients")
+
+# success
+ 
+st.success("Success")
+ 
+# warning
+ 
+st.warning("Warning")
+ 
+# error
+ 
+st.error("Error")
+ 
+# information
+ 
+st.info("Information")
